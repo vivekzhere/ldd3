5ec27a9a4dff3affb2a659ab38b52dbc93009bd7
Vivek Anand T Kallampally <vivekzhere@gmail.com>
2013-11-07 20:16:12 +0530

    Scull in chapter 3 - not working

---
+++ scull/scull.h
@@ -0,0 +1,21 @@
+#ifndef _SCULL_H_
+#define _SCULL_H_
+
+
+#ifndef SCULL_MAJOR
+#define SCULL_MAJOR 0   /* dynamic major by default */
+#endif
+
+#ifndef SCULL_NR_DEVS
+#define SCULL_NR_DEVS 4    /* scull0 through scull3 */
+#endif
+
+#ifndef SCULL_QUANTUM
+#define SCULL_QUANTUM 4000
+#endif
+
+#ifndef SCULL_QSET
+#define SCULL_QSET    1000
+#endif
+
+#endif /* _SCULL_H_ */
